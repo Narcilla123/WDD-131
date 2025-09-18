ca0bc53e254da7efe9e80f73a50bb9b4b58a8121
Nn1fty <arcillaneils391@gmail.com>
2025-09-18 10:18:52 -0600

    class stuff

---
+++ dom_basics/index.js
@@ -0,0 +1,4 @@
+const basicImage = document.createElement("Image");
+basicImage.src = 'https://picsum.photos/200';
+basicImage.setAttribute('alt', 'This is an image placeholder');
+document.append("Image");
---
+++ mission_statement/index.js
@@ -0,0 +1,3 @@
+const apples = 10;
+const oranges = 5;
+console.log(apples + oranges);
