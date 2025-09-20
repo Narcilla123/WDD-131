5293383d874aa536bc2b8aa40fdd59a517d2879e
Nn1fty <arcillaneils391@gmail.com>
2025-09-20 13:08:51 -0600

    completed mission

--- mission_statement/index.js
+++ mission_statement/index.js
@@ -1,3 +1,11 @@
-const apples = 10;
-const oranges = 5;
-console.log(apples + oranges);
+const themeSelector = document.querySelector('select')
+function changeTheme() {
+    if (themeSelector.value === 'dark'){
+        document.body.classList.add('dark');
+        document.querySelector('#logo').src = '/mission_statement/images/black-logo.png';
+    } else {
+        document.body.classList.remove('dark');
+        document.querySelector('#logo').src = '/mission_statement/images/logo.png';
+    }
+}
+themeSelector.addEventListener('change', changeTheme);
