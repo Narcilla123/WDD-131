849970f9a1dcd1ab0e3d17be78d74ae824d540d9
Nn1fty <arcillaneils391@gmail.com>
2025-09-25 09:08:27 -0600

    class_notes

---
+++ wk2/debugging.js
@@ -0,0 +1,7 @@
+const PI = 3.14;
+let radius = 3;
+let area = 0;
+area = radius * radius * PI;
+console.log("Area of circle with radius" + radius + " is " + area)
+radius = 4;
+area = radius * radius * PI;
